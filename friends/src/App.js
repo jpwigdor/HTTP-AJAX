fb2c4817327e525bb67fd99e88c0b30406bfab71
James Wigdor <jpwigdor@gmail.com>
2019-02-05 00:02:15 -0500

    checking git bash is working

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.css'; 
 
 class App extends Component {
   render() {
@@ -11,6 +11,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          
           <a
             className="App-link"
             href="https://reactjs.org"
